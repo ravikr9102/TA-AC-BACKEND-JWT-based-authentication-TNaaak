da40a8cc5f1637cba1e2fb3a0a4be458e1f19e75
Ravi kumar <rkravikr9102@gmail.com>
2022-09-09 14:57:22 +0530

    Done Assignment

---
+++ block-BNaafs/api-auth2/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var auth = require('../middlewares/auth');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/dashboard', auth.verifyToken, (req,res) => {
+  console.log(req.user);
+  res.json({ access: 'protected resource'})
+});
+
+module.exports = router;
